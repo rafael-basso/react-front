efa1c583785da1a41de0c990f751ede610232bf0
rafael-basso <rafael_basso81@hotmail.com>
2024-09-25 08:27:09 -0300

    Retirar timeout

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
   // baseURL: 'http://localhost:3331',
-  baseURL: 'https://react-backend-production-5c9e.up.railway.app/',
-  timeout: 1000
+  baseURL: 'https://react-backend-production-5c9e.up.railway.app/'
 })
 export default api
