6d181505f9bdc585697bc0eb951e1ae01c07a5f4
rafael-basso <rafael_basso81@hotmail.com>
2024-09-25 08:00:05 -0300

    Inserir url prod / ajustar gitignore

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: 'http://localhost:3331',
+  // baseURL: 'http://localhost:3331',
+  baseURL: 'https://react-backend-production-5c9e.up.railway.app/',
   timeout: 1000
 })
 export default api
