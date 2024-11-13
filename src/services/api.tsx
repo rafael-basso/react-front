7daf0d1a03a7ee26fd50b04168d3b5a2c93cc907
rafael-basso <rafael_basso81@hotmail.com>
2024-11-13 11:05:54 -0300

    ajuste url backend

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
   // baseURL: 'http://localhost:3331',
-  baseURL: 'https://react-back-jlporzak4-rafael-bassos-projects.vercel.app'
+  baseURL: 'https://react-back-p2t2ot1gf-rafael-bassos-projects.vercel.app/'
 })
 export default api
